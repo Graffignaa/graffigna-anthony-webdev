54dbc59f867db28f0fd4c33b0ac27fbbea0f3fc1
graffignaa <graffigna.a@husky.neu.edu>
2017-07-24 12:08:36 -0400

    bind controllers to routes and views in config.js

--- public/assignment/js/config.js
+++ public/assignment/js/config.js
@@ -8,43 +8,69 @@
     function Config($routeProvider) {
         $routeProvider
             .when("/login", {
-                templateUrl: "login.view.client.html"
+                templateUrl: "login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
             })
             .when("/", {
-                templateUrl: "login.view.client.html"
+                templateUrl: "login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
             })
             .when("/register", {
-                templateUrl: "register.view.client.html"
+                templateUrl: "register.view.client.html",
+                controller: "RegisterController",
+                controllerAs: "model"
             })
             .when("/user/:uid", {
-                templateUrl: "profile.view.client.html"
+                templateUrl: "profile.view.client.html",
+                controller: "ProfileController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website", {
-                templateUrl: "website-list.view.client.html"
+                templateUrl: "website-list.view.client.html",
+                controller: "WebsiteListController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/new", {
-                templateUrl: "website-new.view.client.html"
+                templateUrl: "website-new.view.client.html",
+                controller: "WebsiteNewController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/wid", {
-                templateUrl: "website-edit.view.client.html"
+                templateUrl: "website-edit.view.client.html",
+                controller: "WebsiteEditController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/wid/page", {
-                templateUrl: "page-list.view.client.html"
+                templateUrl: "page-list.view.client.html",
+                controller: "PageListController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/wid/page/new", {
-                templateUrl: "page-new.view.client.html"
+                templateUrl: "page-new.view.client.html",
+                controller: "PageNewController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/wid/page/pid", {
-                templateUrl: "page-edit.view.client.html"
+                templateUrl: "page-edit.view.client.html",
+                controller: "PageEditController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/wid/page/pid/widget", {
-                templateUrl: "widget-list.view.client.html"
+                templateUrl: "widget-list.view.client.html",
+                controller: "WidgetListController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/wid/page/pid/widget/new", {
-                templateUrl: "widget-list.view.client.html"
+                templateUrl: "widget-new.view.client.html",
+                controller: "WidgetNewController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/wid/page/pid/widget/wgid", {
-                templateUrl: "widget-edit.view.client.html"
+                templateUrl: "widget-edit.view.client.html",
+                controller: "WidgetEditController",
+                controllerAs: "model"
             })
 
 
