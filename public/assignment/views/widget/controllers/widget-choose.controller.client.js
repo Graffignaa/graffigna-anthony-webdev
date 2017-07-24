c2829c12c3f217d01960673c0dcce80bbdd101b5
graffignaa <graffigna.a@husky.neu.edu>
2017-07-24 12:00:27 -0400

    instantiating controllers

--- public/assignment/views/widget/controllers/widget-choose.controller.client.js
+++ public/assignment/views/widget/controllers/widget-choose.controller.client.js
@@ -1,3 +1,13 @@
 /**
  * Created by Anthony on 7/24/2017.
  */
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("WidgetChooseController", WidgetChooseController)
+
+    function WidgetChooseController() {
+
+    }
+
+})();
--- public/assignment/views/widget/controllers/widget-list.controller.client.js
+++ public/assignment/views/widget/controllers/widget-list.controller.client.js
@@ -1,3 +1,13 @@
 /**
  * Created by Anthony on 7/24/2017.
  */
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("WidgetListController", WidgetListController)
+
+    function WidgetListController() {
+
+    }
+
+})();
