8aeddef14a8e0720ef8123ac49629cdc7023fd8b
graffignaa <graffigna.a@husky.neu.edu>
2017-07-30 14:13:46 -0400

    user service server

--- public/assignment/js/app.js
+++ public/assignment/js/app.js
@@ -1,4 +1,14 @@
 (function () {
     angular
         .module("WebAppMaker", ["ngRoute"]);
+
+
+    module.exports = function (app) {
+        require("../services/user.service.client")(app);
+        require("../services/website.service.server.js")(app);
+        require("../services/page.service.server.js")(app);
+        require("../services/widget.service.server.js")(app);
+    };
+
+
 })();
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var app = require('./express');
 var express = app.express;
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 // require("./utilities/filelist");
@@ -10,6 +11,17 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 require("./test/app");
+
+
+var express = require('express');
+var app = express();
+
+// install, load, and configure body parser module
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./public/assignment/js/app")(app);
 
 var port = process.env.PORT || 3000;
 
