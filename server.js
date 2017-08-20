93f056927b2ef42b65fe5b3c6230de997a7d973e
graffignaa <graffigna.a@husky.neu.edu>
2017-08-20 11:06:32 -0400

    assignment

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var express = app.express;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/public'));
 
 require("./test/app");
-//require("./assignment/app")(app);
+require("./assignment/app")(app);
 require("./project/app")(app);
 
 port = process.env.PORT || 3000;
